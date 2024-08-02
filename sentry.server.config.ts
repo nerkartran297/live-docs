36953ead67ef372fdbefdd46e09cb9be1b7f8e15
Khanh Tran <nerkar.tran@gmail.com>
2024-08-02 18:56:58 +0700

    Bug fixes

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,14 +5,15 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-    dsn: "https://391a8a18fc221e951132532efbbcc345@o4507707326922752.ingest.us.sentry.io/4507707329937408",
+  dsn: "https://391a8a18fc221e951132532efbbcc345@o4507707326922752.ingest.us.sentry.io/4507707329937408",
 
-    // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 1,
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
 
-    // Setting this option to true will print useful information to the console while you're setting up Sentry.
-    debug: false,
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
 
-    // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
-    // spotlight: process.env.NODE_ENV === 'development',
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
 });
